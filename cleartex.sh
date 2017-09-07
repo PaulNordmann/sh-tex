2872e794b067f10de6c6648adff8f1f43e53c17e
Paul-Patrick Nordmann <paul-patrick.nordmann@uni-potsdam.de>
2017-09-07 16:31:22 +0200

    add cleartex

---
+++ cleartex.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+if [ $# -eq 0 ]; then
+	dir='.';
+elif [ -d $1 ]; then
+	dir=$1
+else
+	echo 'Verzeichnis als Parameter'
+	exit 0;
+fi
+
+list=`ls $dir/*.tex`; 
+
+for tex in $list 
+do
+	file=${tex%\.tex};
+	rm -f $file.log;
+	rm -f $file.tex.backup;
+	rm -f $file.aux;
+done;
